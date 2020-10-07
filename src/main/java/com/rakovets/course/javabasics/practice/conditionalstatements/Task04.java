cabdffadbac51cf2b3de3c34429a4d56f476fa82
jay <romanmcross@gmail.com>
2020-10-07 16:22:15 +0300

    task 4 done

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task04.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task04.java
@@ -17,9 +17,27 @@ public class Task04 {
         // Код необходимый для тестирования, не изменять
         Scanner scanner = new Scanner(System.in);
         int weekdayNumber = (args.length != 1) ? scanner.nextInt() : Integer.parseInt(args[0]);
-
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        getNameWeekday(weekdayNumber);
+    }
+    private static String getNameWeekday(int weekdayNumber) {
+        String dayNumToTxt = new String();
+        if(weekdayNumber<1||weekdayNumber>7) {
+            dayNumToTxt = null;
+        }else if(weekdayNumber==1){
+            dayNumToTxt = "monday";
+        }else if(weekdayNumber==2){
+            dayNumToTxt = "tuesday";
+        }else if(weekdayNumber==3){
+            dayNumToTxt = "wednesday";
+        }else if(weekdayNumber==4){
+            dayNumToTxt = "thursday";
+        }else if(weekdayNumber==5){
+            dayNumToTxt = "friday";
+        }else if(weekdayNumber==6){
+            dayNumToTxt = "saturday";
+        }else if(weekdayNumber==7){
+            dayNumToTxt = "sunday";
+        }
+        return dayNumToTxt;
     }
 }
