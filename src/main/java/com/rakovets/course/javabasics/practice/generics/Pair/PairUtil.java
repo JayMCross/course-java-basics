9de5243ab6b306d8b1b3be53be810a751a5e45e0
jay <romanmcross@gmail.com>
2020-10-23 14:19:04 +0300

    pair task started

---
+++ src/main/java/com/rakovets/course/javabasics/practice/generics/Pair/PairUtil.java
@@ -0,0 +1,11 @@
+package com.rakovets.course.javabasics.practice.generics.Pair;
+
+public final class PairUtil {
+    public <K,V> void swap(Pair){
+        K variable1 = ;
+        V variable2 = ;
+
+    return ;
+    }
+
+}
